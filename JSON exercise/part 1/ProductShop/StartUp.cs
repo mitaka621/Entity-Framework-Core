5520c9eb2949a68d561bb38b5e40d59576462603
mitaka621 <d418201@gmail.com>
2023-11-20 18:36:47 +0200

    added solutions

---
+++ JSON exercise/part 1/ProductShop/StartUp.cs
@@ -0,0 +1,132 @@
+﻿using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+using System.Diagnostics;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            string usersJSON = File.ReadAllText("../../../Datasets/categories-products.json");
+
+            Console.WriteLine(ImportCategoryProducts(context, usersJSON));
+        }
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<User[]>(inputJson);
+            context.Users.AddRange(users);
+            context.SaveChanges();
+            return $"Successfully imported {users.Length}";
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            var products = JsonConvert.DeserializeObject<Product[]>(inputJson);
+            context.Products.AddRange(products);
+            context.SaveChanges();
+            return $"Successfully imported {products.Length}";
+        }
+
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            var categories = JsonConvert.DeserializeObject<Category[]>(inputJson)
+                .Where(x => x.Name != null).ToArray();
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+            return $"Successfully imported {categories.Length}";
+        }
+
+        public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
+        {
+            var categoryProducts = JsonConvert.DeserializeObject<CategoryProduct[]>(inputJson);
+
+            context.CategoriesProducts.AddRange(categoryProducts);
+            context.SaveChanges();
+
+            return $"Successfully imported {categoryProducts.Length}";
+        }
+
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            var products = context.Products.Select(x => new
+            {
+                name = x.Name,
+                price = x.Price,
+                seller = x.Seller.FirstName + " " + x.Seller.LastName
+            }).Where(x => x.price >= 500 && x.price <= 1000)
+             .OrderBy(x => x.price);
+
+            return JsonConvert.SerializeObject(products, Formatting.Indented);
+        }
+
+        public static string GetSoldProducts(ProductShopContext context)
+        {
+            var users = context.Users.Where(x => x.ProductsSold.Any(z => z.Buyer != null)).Select(x => new
+            {
+                firstName = x.FirstName,
+                lastName = x.LastName,
+                soldProducts = x.ProductsSold.Where(y => y.Buyer != null).Select(y => new
+                {
+                    name = y.Name,
+                    price = y.Price,
+                    buyerFirstName = y.Buyer.FirstName,
+                    buyerLastName = y.Buyer.LastName
+                })
+            }).OrderBy(x => x.lastName).ThenBy(x => x.firstName);
+
+            return JsonConvert.SerializeObject(users, Formatting.Indented);
+        }
+
+        public static string GetCategoriesByProductsCount(ProductShopContext context)
+        {
+
+            var categories = context.Categories.OrderByDescending(x => x.CategoriesProducts.Count).Select(x => new
+            {
+                category = x.Name,
+                productsCount = x.CategoriesProducts.Count,
+                averagePrice = x.CategoriesProducts.Count != 0 ?
+                    $"{x.CategoriesProducts.Average(y => y.Product.Price):F2}"
+                    : "0.00",
+                totalRevenue = x.CategoriesProducts.Count != 0 ?
+                $"{x.CategoriesProducts.Sum(y => y.Product.Price):F2}"
+                : "0.00"
+            });
+
+            return JsonConvert.SerializeObject(categories, Formatting.Indented);
+        }
+
+        public static string GetUsersWithProducts(ProductShopContext context)
+        {
+            var users = context.Users.Where(x => x.ProductsSold.Any(x => x.BuyerId != null)).Select(x => new
+            {
+                firstName = x.FirstName,
+                lastName = x.LastName,
+                age = x.Age,
+                soldProducts = new
+                {
+                    count = x.ProductsSold.Where(x => x.BuyerId != null).Count(),
+                    products = x.ProductsSold.Where(x => x.BuyerId != null).Select(x => new
+                    {
+                        name = x.Name,
+                        price = x.Price
+                    }).ToList(),
+                },
+            }).OrderByDescending(x => x.soldProducts.count).ToArray();
+
+            return JsonConvert.SerializeObject(new 
+            {
+                usersCount = users.Length,
+                users
+            },
+                new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented,
+                    NullValueHandling = NullValueHandling.Ignore
+                });
+        }
+    }
+}
---
+++ JSON exercise/part 2/CarDealer/StartUp.cs
@@ -0,0 +1,199 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs;
+using CarDealer.Models;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+            string file = File.ReadAllText("../../../Datasets/sales.json");
+            Console.WriteLine(GetSalesWithAppliedDiscount(context));
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var supp = JsonConvert.DeserializeObject<Supplier[]>(inputJson);
+
+            context.Suppliers.AddRange(supp);
+            context.SaveChanges();
+            return $"Successfully imported {supp.Length}.";
+        }
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            var Part = JsonConvert.DeserializeObject<Part[]>(inputJson).Where(x=>context.Suppliers.Any(y=>y.Id==x.SupplierId)).ToArray();
+
+            context.Parts.AddRange(Part);
+            context.SaveChanges();
+            return $"Successfully imported {Part.Length}.";
+        }
+
+        public static string ImportCars(CarDealerContext context, string inputJson)
+        {
+            var cars = JsonConvert.DeserializeObject<CarDTO[]>(inputJson);
+
+            foreach (var item in cars)
+            {
+                Car tempCar = new Car()
+                {
+                    Make = item.Make,
+                    Model = item.Model,
+                    TraveledDistance = item.TraveledDistance,
+                    
+                };
+                foreach (var part in item.PartsCars)
+                {
+                    if (context.Parts.Any(p => p.Id == part))
+                    {
+                        tempCar.PartsCars.Add(new PartCar() { PartId = part });
+                    }
+                }
+
+                context.Cars.Add(tempCar);
+            }
+
+
+            context.SaveChanges();
+
+            return $"Successfully imported {cars.Length}.";
+        }
+
+        public static string ImportCustomers(CarDealerContext context, string inputJson)
+        {
+            var Customer = JsonConvert.DeserializeObject<Customer[]>(inputJson);
+
+            context.Customers.AddRange(Customer);
+            context.SaveChanges();
+            return $"Successfully imported {Customer.Length}.";
+        }
+
+        public static string ImportSales(CarDealerContext context, string inputJson)
+        {
+            var Sale = JsonConvert.DeserializeObject<Sale[]>(inputJson);
+
+            context.Sales.AddRange(Sale);
+            context.SaveChanges();
+            return $"Successfully imported {Sale.Length}.";
+        }
+
+        public static string GetOrderedCustomers(CarDealerContext context)
+        {
+           var customers= context.Customers.Select(x => new
+            {
+                Name = x.Name,
+                BirthDate = x.BirthDate,
+                IsYoungDriver = x.IsYoungDriver
+            })
+                .OrderBy(x => x.BirthDate)
+                .ThenBy(x => x.IsYoungDriver);
+            List<object> nice= new List<object>();
+            foreach (var item in customers)
+            {
+                nice.Add(new
+                {
+                    Name=item.Name,
+                    BirthDate = item.BirthDate.ToString("dd/MM/yyyy"),
+                    IsYoungDriver=item.IsYoungDriver
+                });
+            }
+            return JsonConvert.SerializeObject(nice, Formatting.Indented) ;
+        }
+
+        public static string GetCarsFromMakeToyota(CarDealerContext context)
+        {
+            var cars=context.Cars.Where(x => x.Make == "Toyota").OrderBy(x => x.Model).ThenByDescending(x => x.TraveledDistance).Select(x => new
+            {
+                Id = x.Id,
+                x.Make,
+                x.Model,
+                x.TraveledDistance
+            });
+
+            return JsonConvert.SerializeObject(cars, Formatting.Indented);
+        }
+        public static string GetLocalSuppliers(CarDealerContext context)
+        {
+            var sup=context.Suppliers.Where(x => !x.IsImporter).Select(x => new
+            {
+                x.Id,
+                x.Name,
+                PartsCount = x.Parts.Count
+            });
+            return JsonConvert.SerializeObject(sup, Formatting.Indented);
+
+        }
+
+        public static string GetCarsWithTheirListOfParts(CarDealerContext context)
+        {
+            var cars=context.Cars.Select(x => new
+            {
+                car = new
+                {
+                    x.Make,
+                    x.Model,
+                    x.TraveledDistance
+                },
+                parts = x.PartsCars.Select(y => new
+                {
+                    y.Part.Name,
+                    Price = $"{y.Part.Price:F2}"
+                }).ToArray()
+            });
+
+            return JsonConvert.SerializeObject(cars, Formatting.Indented);
+        }
+
+        public static string GetTotalSalesByCustomer(CarDealerContext context)
+        {
+            var result = context.Customers
+               .Where(x => x.Sales.Count > 0)
+               .Select(x => new
+               {
+                   fullName = x.Name,
+                   boughtCars = x.Sales.Count,
+                   spentMoney = x.Sales.Sum(x => x.Car.PartsCars.Sum(x => x.Part.Price)),
+               })
+               .OrderByDescending(x => x.spentMoney)
+               .ThenByDescending(x => x.boughtCars)
+               .ToArray();
+
+            var json = JsonConvert.SerializeObject(result, Formatting.Indented);
+
+           
+
+            return json;
+        }
+
+        public static string GetSalesWithAppliedDiscount(CarDealerContext context)
+        {
+            var result = context.Sales
+               .Take(10)
+               .Select(x => new
+               {
+                   car = new
+                   {
+                       x.Car.Make,
+                       x.Car.Model,
+                       x.Car.TraveledDistance
+                   },
+                   customerName = x.Customer.Name,
+                   discount = x.Discount.ToString("f2"),
+                   price = x.Car.PartsCars.Sum(x => x.Part.Price).ToString("f2"),
+                   priceWithDiscount = (x.Car.PartsCars.Sum(x => x.Part.Price) - x.Car.PartsCars.Sum(x => x.Part.Price) * (x.Discount / 100)).ToString("f2")
+               })
+               .ToArray();
+
+            var json = JsonConvert.SerializeObject(result, Formatting.Indented);
+
+           
+
+            return json.Trim();
+        }
+    }
+}
